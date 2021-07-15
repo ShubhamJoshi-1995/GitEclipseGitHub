ce2862216b1464db2594f438ea1297b556c9f04a
ShubhamJoshi-1995 <72541174+ShubhamJoshi-1995@users.noreply.github.com>
2021-07-15 11:50:41 +0530

    This is update in testcase 2 and testcase 3  commited

--- gitEclipseGitHub/src/testPackage/testCase3.java
+++ gitEclipseGitHub/src/testPackage/testCase3.java
@@ -1,12 +1,11 @@
 package testPackage;
 
-public class testCase2 {
+public class testCase3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Testcase 2 ");
-		System.out.println("New Change in testcase2");
+		System.out.println("This is testcase 3 ");
 
 	}
 
