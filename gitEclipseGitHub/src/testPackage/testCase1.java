f7b102c0ab5bc3b038fd2c9921ed75976c3cba81
ShubhamJoshi-1995 <72541174+ShubhamJoshi-1995@users.noreply.github.com>
2021-07-15 11:22:11 +0530

    Code is created by shubham

---
+++ gitEclipseGitHub/src/testPackage/testCase1.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+public class testCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("This is case 1 ");
+		
+	}
+
+}
