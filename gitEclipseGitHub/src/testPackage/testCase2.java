9918c4360d609178796ec217779ba99474b39e5a
ShubhamJoshi-1995 <72541174+ShubhamJoshi-1995@users.noreply.github.com>
2021-07-15 11:26:28 +0530

    Test case one is modified and testcase 2 is added

--- gitEclipseGitHub/src/testPackage/testCase2.java
+++ gitEclipseGitHub/src/testPackage/testCase2.java
@@ -1,14 +1,12 @@
 package testPackage;
 
-public class testCase1 {
+public class testCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Testcase 2 ");
 
-		
-		System.out.println("This is case 1 ");
-		System.out.println("New Changes ");
-		
 	}
 
 }
